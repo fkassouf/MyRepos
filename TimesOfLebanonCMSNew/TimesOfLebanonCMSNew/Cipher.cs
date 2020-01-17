761c2cd7e2af4de931043119c8f4199a1b64906f
fouad kassouf <mr.kassouf@gmail.com>
2020-01-17 17:41:50 +0200

    pc2

--- TimesOfLebanonCMSNew/TimesOfLebanonCMSNew/Cipher.cs
+++ TimesOfLebanonCMSNew/TimesOfLebanonCMSNew/Cipher.cs
@@ -11,7 +11,7 @@ namespace TimesOfLebanonCMSNew
     public static class Cipher
     {
         /// <summary>
-        /// Encrypt a string.fffffkassouf
+        /// Encrypt a string
         /// </summary>
         /// <param name="plainText">String to be encrypted</param>
         /// <param name="password">Password</param>
